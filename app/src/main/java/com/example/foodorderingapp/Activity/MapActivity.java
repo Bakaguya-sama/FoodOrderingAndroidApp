4c5a48b8f3b0c49cdb0c5ed8ea8859a6bfc43466
TUNGBUI6705 <buitung6705@gmail.com>
2025-05-31 10:08:18 +0700

    map update

---
+++ app/src/main/java/com/example/foodorderingapp/Activity/MapActivity.java
@@ -0,0 +1,37 @@
+package com.example.foodorderingapp.Activity;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.foodorderingapp.R;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapActivity extends AppCompatActivity implements OnMapReadyCallback {
+    private GoogleMap mMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Ví dụ: Di chuyển tới Hà Nội
+        LatLng hanoi = new LatLng(21.0285, 105.8542);
+        mMap.addMarker(new MarkerOptions().position(hanoi).title("Hà Nội"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(hanoi, 12));
+    }
+}
